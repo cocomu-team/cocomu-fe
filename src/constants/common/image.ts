9d3d844d10a821d1397e0f25d114adb3621f9b68
Seio924 <seio9241@gmail.com>
2025-04-10 23:02:04 +0900

    refactor(#189): 이미지 상수 분리

---
+++ src/constants/common/image.ts
@@ -0,0 +1,22 @@
+export const LOGO_IMAGE = 'https://cdn.cocomu.co.kr/images/default/logo.png' as const;
+export const ERROR_IMAGE = 'https://cdn.cocomu.co.kr/images/default/error.png' as const;
+export const LOADING_IMAGE = 'https://cdn.cocomu.co.kr/images/default/loading.png' as const;
+
+export const GOOGLE_IMAGE = 'https://cdn.cocomu.co.kr/images/default/google.png' as const;
+export const GITHUB_IMAGE = 'https://cdn.cocomu.co.kr/images/default/github.png' as const;
+export const KAKAO_IMAGE = 'https://cdn.cocomu.co.kr/images/default/kakao.png' as const;
+
+export const DEFAULT_LANGUAGE_IMAGE = 'https://cdn.cocomu.co.kr/images/languages/java.png' as const;
+
+export const DEFAULT_PROFILE_IMAGE = 'https://cdn.cocomu.co.kr/images/default/profile.png' as const;
+
+export const BANNER_IMAGES = [
+  'https://cdn.cocomu.co.kr/images/banner/banner1.png',
+  'https://cdn.cocomu.co.kr/images/banner/banner2.png',
+  'https://cdn.cocomu.co.kr/images/banner/banner3.png',
+];
+
+export const EMPTY_RESULT_IMAGES = [
+  'https://cdn.cocomu.co.kr/images/default/empty_study.png',
+  'https://cdn.cocomu.co.kr/images/default/empty_space.png',
+] as const;
