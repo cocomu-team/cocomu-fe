cf7931b0a6b9b00819430608865f0a0e2fc67cd2
beomjo <dongjun1315@naver.com>
2025-04-14 14:58:40 +0900

    feat(#195): 스토리북 메인 설정 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,39 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
-    '@storybook/addon-themes'
+    '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+      define: {
+        global: 'window',
+      },
+    });
+  },
+  swc: () => ({
+    jsc: {
+      transform: {
+        react: {
+          runtime: 'automatic',
+        },
+      },
+    },
+  }),
+  staticDirs: ['../public'],
 };
 export default config;
